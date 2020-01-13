b676e26ca86b7fea268e279be5b0861977b801fe
songgisung <sgs5789@naver.com>
2020-01-13 20:19:21 +0900

    勉強CHAPTER01

---
+++ spring_base/chapter01/SpringChap/src/main/java/com/sgs/app/chap01/Main.java
@@ -0,0 +1,26 @@
+package com.sgs.app.chap01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		MavenBuildRunner buildRunner = new MavenBuildRunner();
+		buildRunner.setMavenPath("c:\\apache-maven-3.1.1");
+		
+		Project sampleProject = new Project();
+		List<String> srcDirs = new ArrayList<>();
+		
+		srcDirs.add("src");
+		srcDirs.add("srcResources");
+		
+		sampleProject.setSrcDirs(srcDirs);
+		sampleProject.setBinDir("bin");
+		sampleProject.setBuildRunner(buildRunner);
+		
+		sampleProject.build();
+		
+	}  
+}
